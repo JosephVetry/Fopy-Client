4f3bec477cd8887bbf376362119a92a989a19997
Joseph Vetry <josefvetry974@gmail.com>
2023-07-01 16:31:45 +0700

    LandingPage

---
+++ landingPage/sildes.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: '1',
+        title: 'Quick & Easy Payments',
+        description: 'Grow your business by accepting card payments with the new card reader.',
+        image: require('./assets/images/creditCard.png')
+    },
+    {
+        id: '2',
+        title: 'Smart Way to Work',
+        description: ' Complete point of sale software tailrode to your business needs.',
+        image: require('./assets/images/print.png')
+    },
+    {
+        id: '3',
+        title: 'Instant Notifications',
+        description: 'Instant notifications let your quickly see news purchases and messages.',
+        image: require('./assets/images/notif.png')
+    },
+    {
+        id: '4',
+        title: 'Customize Everything',
+        description: 'Adjust your system to speed up your checkout.',
+        image: require('./assets/images/customize.png')
+    },
+]
